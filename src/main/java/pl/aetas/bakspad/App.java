cef1e144e119d830b9807e01020d399f155cce03
mariusz <mariusz@aetas.pl>
2014-03-08 19:12:27 +0000

    Catching all non catched exceptions and closing app when fatal error occurs

--- src/main/java/pl/aetas/bakspad/App.java
+++ src/main/java/pl/aetas/bakspad/App.java
@@ -6,6 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import jfxtras.labs.dialogs.MonologFX;
+import jfxtras.labs.dialogs.MonologFXBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import pl.aetas.bakspad.presentation.controller.MainController;
 
 public class App extends Application {
@@ -14,18 +18,29 @@ public class App extends Application {
 
     private MainController mainController;
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("main.fxml"));
-        stage.setTitle(MAIN_WINDOW_TITLE);
-        stage.setScene(new Scene((Parent)loader.load()));
-        mainController = loader.getController();
-        mainController.setStage(stage);
-        stage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("main.fxml"));
+            stage.setTitle(MAIN_WINDOW_TITLE);
+            stage.setScene(new Scene((Parent)loader.load()));
+            mainController = loader.getController();
+            mainController.setStage(stage);
+            stage.show();
+        } catch (Exception e) {
+            LOGGER.fatal("Something went really wrong. Closing application.", e);
+            stop();
+        }
     }
 
     @Override
     public void stop() throws Exception {
+        MonologFX errorDialog = MonologFXBuilder.create().type(MonologFX.Type.ERROR).titleText("Poważny błąd programu")
+                .message("Skontaktuj się z twórcą programu. Program zostanie zamknięty.").modal(true).build();
+
+        errorDialog.showDialog();
         mainController.handleCloseApplicationAction(new ActionEvent());
         super.stop();
     }
